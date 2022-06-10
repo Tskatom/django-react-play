3ff48a9d2ed6fc36c44303599d331a908a90da64
Wei Wang <wwang@qualtrics.com>
2022-06-09 21:45:42 -0700

    update

---
+++ apps/users/managers.py
@@ -0,0 +1,54 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+class CustomUserManager(BaseUserManager):
+
+    def email_validator(self, email):
+        try:
+            validate_email(email)
+        except ValidationError:
+            raise ValueError(_("You must provide a valid email"))
+
+
+    def create_user(self, username, first_name, last_name, email, password, **extra_fields):
+        if not username:
+            raise ValueError(_("You must provide user name"))
+        
+        if not first_name:
+            raise ValueError(_("You must provide first name"))
+        
+        if not last_name:
+            raise ValueError(_("You must provide last name"))
+        
+        if email:
+            email = self.normalize_email(email)
+            self.email_validator(email)
+        else:
+            raise ValueError(_("Email address is required"))
+
+        user = self.model(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            **extra_fields
+        )
+
+        user.set_password(password)
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, username, first_name, last_name, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if not password:
+            raise ValueError(_("superuser must have password."))
+        
+        user = self.create_user(username, first_name, last_name, email, password, **extra_fields)
+        return user
